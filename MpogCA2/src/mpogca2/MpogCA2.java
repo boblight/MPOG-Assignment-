317d253b7e18cb734f853f775d47623cbbcd0f95
tongliang <tongliang@TL>
2016-07-07 10:26:32 +0800

    Added main menu elements.

--- MpogCA2/src/mpogca2/MpogCA2.java
+++ MpogCA2/src/mpogca2/MpogCA2.java
@@ -5,48 +5,94 @@
  */
 package mpogca2;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author tongliang
  */
 public class MpogCA2 extends Application {
-    private Button host, join, help, setting, exit;
+
+    private Button host, join, help, exit;
     private Label mainTitle;
-    
+    private Stage currentStage;
+
+    final static AudioClip bPush = new AudioClip(new File("src/buttonPush.wav").toURI().toString());
+
     @Override
     public void start(Stage primaryStage) {
+        Action(primaryStage, createMainMenu(), "Main Menu");
         
-        
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        exit.setOnAction(e -> {
+           bPush.play();
+           System.exit(0);
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 400, 600);
-        
-        primaryStage.setTitle("Main menu");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }//end of main javafx class
-    
-    
+
+    //create main menu
+    public Scene createMainMenu() {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 860, 660);
+
+        VBox vbCenter = new VBox(15);
+        vbCenter.setAlignment(Pos.CENTER);
+
+        mainTitle = new Label("Ooorrbbs");
+        host = new Button("Host lobby");
+        join = new Button("Join lobby");
+        help = new Button("How to play");
+        exit = new Button("Exit");
+        
+        vbCenter.getChildren().add(mainTitle);
+        vbCenter.getChildren().add(host);
+        vbCenter.getChildren().add(join);
+        vbCenter.getChildren().add(help);
+        vbCenter.getChildren().add(exit);
+        
+        root.setCenter(vbCenter);
+        
+        return scene;
+    }//end of createMainMenu
+
+    //change screen
+    public void Action(Stage stage, Scene scene, String title) {
+        currentStage = stage;
+
+        if (scene == null) {
+            bPush.play();
+            stage.close();
+        }
+
+        currentStage.setScene(scene);
+        currentStage.setTitle(title);
+
+        //currentStage.getIcons().add(new Image("logo.png"));
+        currentStage.show();
+
+        currentStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+
+                t.consume();
+                //Action(currentStage, createMainMenu(), "Main Menu");
+            }
+        });
+    }//end of generic button listener
 
     /**
      * @param args the command line arguments
@@ -54,5 +100,5 @@ public class MpogCA2 extends Application {
     public static void main(String[] args) {
         launch(args);
     }//end of main method
-    
+
 }
