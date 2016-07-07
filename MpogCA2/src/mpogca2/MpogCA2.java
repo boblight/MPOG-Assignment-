e2041e28adb05d8a21c9b624af337e25c811ea2b
tongliang <tongliang@TL>
2016-07-07 10:53:48 +0800

    Menu stuff;

--- MpogCA2/src/mpogca2/MpogCA2.java
+++ MpogCA2/src/mpogca2/MpogCA2.java
@@ -15,8 +15,11 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
@@ -29,7 +32,9 @@ import javafx.stage.WindowEvent;
  */
 public class MpogCA2 extends Application {
 
-    private Button host, join, help, exit;
+    private Button host, join, help, exit, startGame;
+    private TextArea chatArea;
+    private TextField chatMsg;
     private Label mainTitle;
     private Stage currentStage;
 
@@ -46,7 +51,20 @@ public class MpogCA2 extends Application {
         
     }//end of main javafx class
 
-    //create main menu
+    //create lobby
+    public Scene createLobby() {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 1000, 500);
+        
+        HBox h = new HBox(15);
+        VBox v = new VBox(15);
+        
+        
+        
+        return null;
+    }//end of createLobby
+    
+    //create main menu ui
     public Scene createMainMenu() {
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 860, 660);
